5f7b7f229252e6c8318875146186e6307d54f873
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2023-12-19 10:39:26 +0530

    created the store

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import UserModule from './Components/userModule';
+import { makeAutoObservable } from 'mobx';
+
+export class UserModalViewStore {
+  opened = false;
+  constructor() {
+    makeAutoObservable(this,undefined,{autoBind:true});
+  }
+  open() {
+    this.opened= true;
+  }
+  close(){
+    this.opened=false;
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/users/*' element={<UserModule/>}/>
+    <Route path='/' element={<Link to="/users" >Go to the Users</Link>} />
+   </Routes>
+   </BrowserRouter>
   );
 }
 
